ec1bb49394c739b2ff4712284ef0c1fb7fdb93af
Nika Babych <nanoornika@gmail.com>
2023-05-10 23:17:21 +0300

    clean old and useless files

---
+++ main.py
@@ -0,0 +1,47 @@
+"""Автоматичне укладання вправ із граматики української мови як іноземної"""
+
+
+import stanza
+import sqlite3
+import tkinter as tk
+
+
+class SQL:
+    """Містить SQL-запити та методи створення БД вправ."""
+
+    def __init__(self) -> None:
+        pass
+
+
+class Word:
+    """"""
+
+
+class Pronoun(Word):
+    """"""
+
+
+class Sentence:
+    """"""
+
+
+class Text:
+    """"""
+
+
+class Executor:
+    """"""
+
+
+class Body(tk.Frame):
+    """"""
+
+
+class Application(tk.Tk):
+    """"""
+
+
+if __name__ == '__main__':
+    app = Application()
+    app.mainloop()
+    con.close()
--- task_generator.py
+++
@@ -1,3 +0,0 @@
-from config import task_grading_executor
-
-task_grading_executor()
