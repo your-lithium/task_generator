053d61aba32d8bb4a8b1f43f54e4a3f172dedcac
Nika Babych <nanoornika@gmail.com>
2023-05-12 22:44:14 +0300

    Add reading DB v0.2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import stanza
 import sqlite3
 import tkinter as tk
+import random
 
 
 class SQL:
@@ -13,27 +14,46 @@ class SQL:
         con = sqlite3.connect('tasks.db')
         self.cur = con.cursor()
 
-    def tasks_choice(self, pos: str, set_number: int) -> list[int]:  # + level
+    def tasks_choice(self, level: str, pos: str, set_number: int) -> list[int]:
         """Створює список вправ, які підходять під вимоги користувача"""
 
-        self.cur.execute(f"""SELECT sentence_id
-                             FROM sentences_sets
-                             WHERE set_id = {set_number}""")
-        set_sents_list = tuple(list(row)[0] for row in self.cur.fetchall())
-        print(set_sents_list)
+        self.cur.execute(f"""SELECT *
+                             FROM {pos}_levels
+                             WHERE level_id = {level}""")
+        level_list = [[description[0] for description in self.cur.description[1:]][i]
+                      for i, bool in enumerate(self.cur.fetchall()[0][1:]) if bool == 1]
 
-        self.cur.execute(f"""SELECT sentence_id
+        self.cur.execute(f"""SELECT text, sentence_id, token_index, pos_id, {", ".join(level_list)}
                              FROM tokens
+                             INNER JOIN {pos}s
+                                ON tokens.pos_id = {pos}s.{pos}_id
                              WHERE pos = '{pos}'
-                                AND sentence_id IN {set_sents_list}""")
-        pos_sents_list = tuple(list(row)[0] for row in self.cur.fetchall())
-        print(pos_sents_list)
+                                AND sentence_id IN
+                                    (SELECT sentence_id
+                                     FROM sentences_sets
+                                     WHERE set_id = {set_number})
+                                AND form IN {tuple(level_list)}""")
+        corrects_list = [list(row) for row in self.cur.fetchall()]
+        print(corrects_list)
 
-        self.cur.execute(f"""SELECT text, sentence_id, token_index, pos, pos_id
+        self.cur.execute(f"""SELECT text, sentence_id, token_index
                              FROM tokens
-                             WHERE sentence_id IN {pos_sents_list}""")
-        tokens_list = [list(row) for row in self.cur.fetchall()]
-        print(tokens_list)
+                             WHERE pos != '{pos}'
+                                   AND sentence_id IN
+                                       (SELECT sentence_id
+                                        FROM tokens
+                                        WHERE pos = '{pos}'
+                                            AND sentence_id IN
+                                                (SELECT sentence_id
+                                                 FROM sentences_sets
+                                                 WHERE set_id = {set_number}))""")
+        stems_list = [list(row) for row in self.cur.fetchall()]
+        print(stems_list)
+
+        # duplicates = []
+        # duplicate_check = set([correct[1] for correct in corrects_list])
+        # if len(duplicate_check) < len(corrects_list):
+
 
 
 class Word:
